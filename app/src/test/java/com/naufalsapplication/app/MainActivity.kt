4b13cb0c214bf04a015042903911687fc59e7da8
Nexuszzz <naufalmuhammaddzaka@gmail.com>
2024-01-02 15:48:07 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/naufalsapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.naufalsapplication.app
+
+import com.naufalsapplication.app.appcomponents.base.BaseActivity
+import com.naufalsapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
